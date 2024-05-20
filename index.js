81c20b5f6518c01ec214f4b58055e65e00115a39
Adnan3707 <adnanfarooq37@gmail.com>
2024-05-20 11:45:35 +0530

    streams working

---
+++ index.js
@@ -0,0 +1,56 @@
+const fs = require('fs');
+const csv  = require('csvtojson');
+const { Transform } = require('stream');
+const { error } = require('console');
+const main= () =>{
+    // const readStream = fs.createReadStream('./annual-enterprise-survey-2021-financial-year-provisional-csv.csv');
+    const readStream = fs.createReadStream('./import.csv');
+    const writeStream = fs.createWriteStream('./write.csv')
+    // readStream.on('data',(buffer)=>{
+    //     console.log('DATA>>>');
+    //     console.log(buffer);
+    //     writeStream.write(buffer);
+    // })
+    // readStream.on('end',()=>{
+    //     console.log('Stream Ended');
+    //     writeStream.end()
+    // })
+    readStream.pipe(writeStream)
+    
+    // readStream.pipe(
+    //     csv({
+    //         delimiter:';'
+    //     },{objectMode:true})
+    // ).on('data',data=>{
+    //     console.log(data)
+    // }) 
+
+    const myTransform = new Transform({
+            objectMode:true,
+            transform(chunk,enc,callback){
+            console.log('>> Chunk ',chunk);
+            const user ={
+                name:chunk.name,
+                email: chunk.email.toLowerCase(),
+                age: Number(chunk.age),
+                isActive:chunk.isActive === 'true'
+            }
+            callback(null,user)    
+            }
+        })
+
+    readStream.pipe(
+        csv({
+            delimiter:';'
+        },{objectMode:true})
+    ).pipe(myTransform).on('data',data=>{
+        console.log('--Data ');
+        console.log(data)
+    }).on('error',error=>{
+        console.log('error',error)
+    }).on('end',()=>{
+        console.log('stream ended')
+    })
+    
+}
+main()
